2b523261a23e16cd344e2a5d8e99f86c9256801a
Erlon F Souza <erlon.efs@gmail.com>
2018-07-17 08:21:09 -0300

    Add Handler para AlunoMatriculado

---
+++ src/GestaoEscolar/Demo.GestaoEscolar.Handlers/Alunos/AlunoMatriculadoHandler.cs
@@ -0,0 +1,22 @@
+﻿using Demo.GestaoEscolar.Domain.Aggregates.Alunos;
+using Demo.GestaoEscolar.Domain.Repositories.Alunos;
+using SharedKernel.Common;
+using System.Threading.Tasks;
+
+namespace Demo.GestaoEscolar.Handlers.Alunos
+{
+	public class AlunoMatriculadoHandler : IHandler<AlunoMatriculado>
+	{
+		private IAlunoRepository _alunoRepository;
+
+		public AlunoMatriculadoHandler(IAlunoRepository alunoRepository)
+		{
+			_alunoRepository = alunoRepository;
+		}
+
+		public async Task HandleAsync(AlunoMatriculado e)
+		{
+
+		}
+	}
+}
