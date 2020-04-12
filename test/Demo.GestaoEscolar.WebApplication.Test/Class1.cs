19f3051ccd68331a645985521b7af55c1e346767
Erlon F Souza <erlon.efs@gmail.com>
2020-04-12 10:03:55 -0300

    Add WebApplicationTest

---
+++ test/Demo.GestaoEscolar.WebApplication.Test/Class1.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Demo.GestaoEscolar.WebApplication.Test
+{
+	public class Class1
+	{
+
+		[Fact]
+		public void hello_world()
+		{
+			true.Should().BeTrue();
+		}
+	}
+}
